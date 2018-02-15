0848e7bf3a1e3024cf38d0fedf6d0f42d264239e
danielb <danielbarbero@bewanted.com>
2018-02-15 13:16:01 +0100

    ADD dinamic content with curlybraces

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ const Card = ({}) => (
   <div>
     <h3>Mago</h3>
     <p>Health  &lt;3 : 100</p>
-    <p>Power (ﾉ´･ω･)ﾉ ﾐ ┸━┸: 100</p>
+    <p>Power (ﾉ´･ω･)ﾉ ﾐ ┸━┸: { Math.floor(Math.random() * 100) }</p>
   </div>
 );
 
