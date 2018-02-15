0860eff258c2660e888baf49949e7add0b62fb92
danielb <danielbarbero@bewanted.com>
2018-02-15 13:11:38 +0100

    ADD static Card stateless component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({}) => (
+  <div>
+    <h3>Mago</h3>
+    <p>Health  &lt;3 : 100</p>
+    <p>Power (ﾉ´･ω･)ﾉ ﾐ ┸━┸: 100</p>
+  </div>
+);
+
+export default Card;
