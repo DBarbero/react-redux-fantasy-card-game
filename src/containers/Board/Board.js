b8d2abc5f479aaf6965a1f43546625de2ff034a8
danielb <danielbarbero@bewanted.com>
2018-02-16 14:54:50 +0100

    ADD rendering list dynamically

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -7,28 +7,28 @@ class Board extends Component {
     super(props);
     this.state = {
       cards: [
-        { name: 'Gandalf', race: 'Mage', health: '100', power: '80' },
-        { name: 'Frodo', race: 'Hobbit', health: '100', power: '25' },
-        { name: 'Gollum', race: 'Hobbit', health: '100', power: '25' },
-        { name: 'Legolas', race: 'Elf', health: '300', power: '75' },
+        { id: 'c001', name: 'Gandalf', race: 'Mage', health: '100', power: '80' },
+        { id: 'c002', name: 'Frodo', race: 'Hobbit', health: '100', power: '25' },
+        { id: 'c003', name: 'Gollum', race: 'Hobbit', health: '100', power: '25' },
+        { id: 'c004', name: 'Legolas', race: 'Elf', health: '300', power: '75' },
       ],
       showCards: false
     }
 
   }
 
-  handleHitCharacter = (index) => {
-    const newCards = this.state.cards.map( (card, i) => {
-      if (i === index)
-        return { ...card, health: card.health - 20}
-      return card
-    })
+  handleHitCharacter = (id) => {
+    const characterIndex = this.state.cards.findIndex( card => card.id === id)
+    const oldCharacter = this.state.cards[characterIndex]
+    const newCharacter = { ...oldCharacter, health: oldCharacter.health - 20}
+    const newCards = [...this.state.cards]
+    newCards[characterIndex] = newCharacter
     this.setState({ cards: newCards });
   }
 
-  handleOnChangeName = (event, index) => {
-    const newCards = this.state.cards.map( (card, i) => {
-      if (i === index)
+  handleOnChangeName = (event, id) => {
+    const newCards = this.state.cards.map( card => {
+      if (card.id === id)
         return { ...card, name: event.target.value}
       return card
     })
@@ -45,22 +45,12 @@ class Board extends Component {
     const { cards } = this.state
     const CardsComponent = (
       <div>
-        <Card character={cards[0]}
-          onHitCard={() => this.handleHitCharacter(0)}
-          onChangeName={(e) => this.handleOnChangeName(e, 0)}
-        ></Card>
-        <Card character={cards[1]}
-           onHitCard={() => this.handleHitCharacter(1)}
-           onChangeName={(e) => this.handleOnChangeName(e, 1)}
-        ></Card>
-        <Card character={cards[2]}
-          onHitCard={() => this.handleHitCharacter(2)}
-          onChangeName={(e) => this.handleOnChangeName(e, 2)}
-        ><h3>My Precius!</h3></Card>
-        <Card character={cards[3]}
-          onHitCard={() => this.handleHitCharacter(3)}
-          onChangeName={(e) => this.handleOnChangeName(e, 3)}
-        ></Card>
+        { cards.map( card =>
+          <Card key={card.id} character={card}
+            onHitCard={() => this.handleHitCharacter(card.id)}
+            onChangeName={ (e) => this.handleOnChangeName(e, card.id) }
+          ></Card>
+        ) }
       </div>
     )
     return (
